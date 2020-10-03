31cbe8c747fbf74133c1a5997a8bcb7523ec13c9
s10 <68397573+s10-n@users.noreply.github.com>
2020-10-02 23:50:50 -0700

    added comments to code

--- sip_bot.py
+++ sip_bot.py
@@ -6,11 +6,14 @@ import discord
 from dotenv import load_dotenv
 from discord.ext import commands
 
+# load the bot's Discord token from the .env file
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
+# create the bot
 bot = commands.Bot(command_prefix='!')
 
+# when a user calls the command !sip followed by a key phrase, the bot returns the description provided by get_description()
 @bot.command(name='sip', help='Prefaces a description of the keyword with a big sip')
 async def sips(ctx, *keywords):
     key_phrase = ' '.join(keywords)
--- wikipedia_grabber.py
+++ wikipedia_grabber.py
@@ -4,19 +4,29 @@ import re
 import wikipedia
 
 def get_description(message_content):
+
+    # if there are no search results on Wikipedia for the message content, return an answer to that effect
     search_results = wikipedia.search(message_content)
     if not search_results:
         return "something I've never heard of."
+
+    # get the first search result and look up the summary for it
     else:
         search_result = search_results[0]
         try:
             wiki_description = wikipedia.summary(search_result)
+
+            # find the description in the first sentence of the paragraph and remove the conjugated form of "be"
             description = re.search(' was [^\.]+\.| are [^\.]+\.| were [^\.]+\.| is [^\.]+\.', wiki_description).group()
             return re.sub('\A were |\A was |\A are |\A is ','', description)
+
+        # if the page is a disambiguation, get the first result from the disambiguation page
         except wikipedia.exceptions.DisambiguationError as e:
             try:
                 wiki_description = wikipedia.summary(re.sub('[()]','',e.options[0]))
                 description = re.search(' was [^\.]+\.| are [^\.]+\.| were [^\.]+\.| is [^\.]+\.', wiki_description).group()
                 return re.sub('\A were |\A was |\A are |\A is ','', description)
+
+            # if that doesn't work (as seen in some very vague search terms), ask for more detail
             except AttributeError:
                 return "a case where you'll have to be more specific."
